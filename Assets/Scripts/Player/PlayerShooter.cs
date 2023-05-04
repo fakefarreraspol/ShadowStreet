0f2e8ef0dba8ddc61040f60aef14f0a5024a81df
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-05-04 12:57:16 +0200

    new script

---
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerShooter : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private Transform attackPoint;
+    [SerializeField]
+    private Camera fpsCam;
+    
+    [Header("Shoot Settings")]
+    [SerializeField]
+    private float shootForce, upwardForce;
+    [SerializeField]
+    private float timeBetweenShooting, spread, reloadTime, timeBetweenShoots;
+    [SerializeField]
+    private int magazineSize, bulletsPerTap;
+    [SerializeField]
+    public bool allowButtonHold;
+
+    [Header("Graphics")]
+    [SerializeField]
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //bug fixing
+    public bool allowInvoke = true;
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        //Check if allowed to hold button
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //Reloading
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) Reload();
+
+
+
+        //Shooting
+        if(readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+
+        //Check exact hit
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else targetPoint = ray.GetPoint(75); //Point far from player
+
+        //direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //Calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate new direction with spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+
+        //Instantiate bullet
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        //Rotate bullet to shoot direction
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        //AddForces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        //Instantiate muzzel flash
+        if (muzzleFlash != null)
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+    }
+
+    private void ResetShot()
+    {
+        //Allow shooting and invoking again
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
