d7522c1201cf3ab31e7f294ebfc0e0d32f8ebc1e
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-05-31 12:32:57 +0200

    now cars appear

--- Assets/Scripts/Managers/CarSpawner.cs
+++ Assets/Scripts/Managers/CarSpawner.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform[] spawners;
+    public GameObject[] cars;
+    
+    private int randomSpawn;
+    
     void Start()
     {
-        
+        InvokeRepeating("SpawnCar", 1, 10);       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         
+    }
+
+    void SpawnCar()
+    {
+        randomSpawn = Random.Range(0, spawners.Length);
+
+        if(randomSpawn == 1)
+        {
+            Instantiate (cars[0], spawners[randomSpawn].transform.position, Quaternion.Euler(0,180,0));
+        }
+        else Instantiate (cars[0], spawners[randomSpawn].transform.position, Quaternion.identity);
+        // Instantiate (cars[0], spawners[randomSpawn].transform.position, Quaternion.Euler(0,180,0));
     }
 }
