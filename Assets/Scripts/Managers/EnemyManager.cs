0591ab26a3889c908f148106a6f007a2e2aaf2d8
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-06-04 18:42:28 +0200

    mode 01 done

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -7,15 +7,29 @@ public class EnemyManager : MonoBehaviour
     // Start is called before the first frame update
     public Transform[] spawnpoint;
     public GameObject[] enemy;
+
+    public bool mode02 = false;
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 1, 5);
+        if (mode02) InvokeRepeating("SpawnEnemyRandom", 1, 5);
+        else
+        {
+            for (int i = 0; i < spawnpoint.Length; i++)
+            {
+                SpawnEnemy(i);
+            }
+        }
+
     }
 
     // Update is called once per frame
-    void SpawnEnemy()
+    void SpawnEnemyRandom()
     {
         int number = Random.Range(0, spawnpoint.Length);
         Instantiate(enemy[0], spawnpoint[number].position, Quaternion.identity);
+    }
+    void SpawnEnemy(int numb)
+    {
+        Instantiate(enemy[0], spawnpoint[numb].position, Quaternion.identity);
     }
 }
