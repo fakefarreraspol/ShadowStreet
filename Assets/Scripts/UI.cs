72b96975270a20f680ee33fb1d8a227a61d0772a
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-06-05 13:17:07 +0200

    Ui with score

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,51 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 //using UnityEngine.UIElements;
 public class UI : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool mod002;
-    Player player;
-    Image panel;
+    // Player player;
+    // Image panel;
+    public TextMeshProUGUI scoreDisplay;
+    public ScoreScript scScr;
+    public string paco;
     void Start()
     {
-        player = FindObjectOfType<Player>();
-        panel = GetComponent<Image>();
+        // player = FindObjectOfType<Player>();
+        // panel = GetComponent<Image>();
+        scoreDisplay = GetComponent<TextMeshProUGUI>();
+        scScr = FindObjectOfType<ScoreScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.health < player.maxHealth/4)
-        {
-            var tempColor = panel.color;
-            tempColor.a = 1f;
-            panel.color = tempColor;
-        }
-        else if (player.health < player.maxHealth/3)
-        {
-            var tempColor = panel.color;
-            tempColor.a = 0.75f;
-            panel.color = tempColor;
-        }
-        else if (player.health < player.maxHealth/2)
-        {
-            var tempColor = panel.color;
-            tempColor.a = 0.5f;
-            panel.color = tempColor;
-        }
-        else if (player.health < player.maxHealth/1.5)
-        {
-            var tempColor = panel.color;
-            tempColor.a = 0.25f;
-            panel.color = tempColor;
-        }
-        else
-        {
-            var tempColor = panel.color;
-            tempColor.a = 0.0f;
-            panel.color = tempColor;
-        }
+        scoreDisplay.text = scScr.GetPoints().ToString();
+        //paco = scScr.GetPoints().ToString();
+        // if(player.health < player.maxHealth/4)
+        // {
+        //     var tempColor = panel.color;
+        //     tempColor.a = 1f;
+        //     panel.color = tempColor;
+        // }
+        // else if (player.health < player.maxHealth/3)
+        // {
+        //     var tempColor = panel.color;
+        //     tempColor.a = 0.75f;
+        //     panel.color = tempColor;
+        // }
+        // else if (player.health < player.maxHealth/2)
+        // {
+        //     var tempColor = panel.color;
+        //     tempColor.a = 0.5f;
+        //     panel.color = tempColor;
+        // }
+        // else if (player.health < player.maxHealth/1.5)
+        // {
+        //     var tempColor = panel.color;
+        //     tempColor.a = 0.25f;
+        //     panel.color = tempColor;
+        // }
+        // else
+        // {
+        //     var tempColor = panel.color;
+        //     tempColor.a = 0.0f;
+        //     panel.color = tempColor;
+        // }
     }
 }
